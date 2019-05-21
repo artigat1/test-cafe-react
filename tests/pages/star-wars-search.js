1280e430013d24d6fd1abf743cb9d384b50bb7e5
Steve Doggett <artigat1@users.noreply.github.com>
2019-05-21 10:52:46 +0100

    Trying to run on CircleCI

--- tests/pages/star-wars-search.js
+++ tests/pages/star-wars-search.js
@@ -1,26 +1,31 @@
-import { RequestLogger } from 'testcafe';
+import {RequestLogger} from 'testcafe';
 
 import StarWarsSearchPage from '../page-object/star-wars-search.page';
 
-// const swapiLogger = RequestLogger('https://swapi.co/api/people/', {
-//     logRequestHeaders: true,
-// });
+const logger = RequestLogger(
+    {url: 'https://swapi.co/api/people/', method: 'get'},
+    {
+        logRequestHeaders: true,
+        logResponseHeaders: true
+    });
 
 const page = new StarWarsSearchPage();
 
-fixture`Test the Star Wars character search`
-    .page('http://localhost:3000');
+fixture `Test the Star Wars character search`
+    .page('http://localhost:3000')
+    .requestHooks(logger);
 
-// test.requestHooks(swapiLogger)
-// ('Test outgoing swapi requests', async t => {
-//     await t
-//         .typeText(queryInput, 'l')
-//         .expect(swapiLogger.count(() => true)).eql(1);
-// });
-
-test('Expect 1 results when typing "luke', async t => {
+test
+('Expect 1 results when typing "luke', async t => {
     await page.typeQuery('luke');
     await page.results.with({visibilityCheck: true});
-    
+
     await t.expect(page.results.count).eql(1);
+});
+
+test('Expect 3 results when typing "skywalker', async t => {
+    await page.typeQuery('skywalker');
+    await page.results.with({visibilityCheck: true});
+
+    await t.expect(page.results.count).eql(3);
 });
